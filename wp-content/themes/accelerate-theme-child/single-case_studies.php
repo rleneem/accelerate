0763a6c7c21456b7923db99fcd7aa03cdf540f08
Rleneem <rleneem@gmail.com>
2016-12-31 12:33:15 -0800

    update to case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,13 +14,52 @@
 
 get_header(); ?>
 
+
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<article class="case-study">
-				<?php the_content(); ?>
-			</article>	
-			<?php endwhile; // end of the loop. ?>
+		
+	<?php while ( have_posts() ) : the_post();
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image_1 = get_field('image_1');
+		$image_2 = get_field('image_2');
+		$image_3 = get_field('image_3');
+		$size = "full";
+	?>
+			
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><?php the_title(); ?></h2>
+			<h5><?php echo $services; ?></h5>
+			<h6>Client: <?php echo $client; ?></h6>
+				
+			<?php the_content(); ?>
+			
+			<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+				
+		</aside>
+			
+		<div class="case-study-images">
+			<?php if(image_1) { ?>
+				<?php echo wp_get_attachment_image(
+				$image_1, $size) ; ?>
+			<?php } ?>
+				<?php echo wp_get_attachment_image(
+				$image_2, $size) ; ?>
+			<?php if(image_1) { ?>
+				<?php echo wp_get_attachment_image(
+				$image_3, $size) ; ?>
+			<?php } ?>
+			
+			<?php if(image_1) { ?>
+				<img src="<?php echo $image_3; ?>" />
+			<?php } ?>	
+		</div>
+			
+	</article>
+	
+	<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
