2975baddca0552d0d5a320fa76c259fb9e284383
Rleneem <rleneem@gmail.com>
2017-01-27 11:25:22 -0800

    more on 404

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -12,17 +12,44 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			<div class="404-page">
-				<div class="404_image">
-					<figure>
+		
+			<header class="page-header">
+				<figure>
 						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/lionel.jpg"/> 
-					</figure>
-				</div>
+				</figure>
+			</header>
+			
+			<div class="404-page">
 				<div class="text_404">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?'); ?></p>
+					<h2><?php _e( 'Oh, it isn&rsquo;t? Ok well, this is somewhat embarrassing.', 'skillcrushstarter' ); ?></h2>
+					<p><?php _e( 'Maybe you could try a search to find what it is you are truly looking for?'); ?></p>
 					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
+				</div>
+				
+				<h3>Or, you could check out some of our popular content:</h3>
+				
+				<div class="col1">
+					<div class="col-header">
+						<h3>Popular Posts</h3>
+					</div>
+					<?php wpp_get_mostpopular(); ?>
+				</div>
+				
+				<div class="col2">
+					<div class="col-header">
+						<h3>Random Posts</h3>
+					</div>	
+						<ul>
+						<?php
+						$posts = get_posts('orderby=rand&numberposts=5');
+							foreach($posts as $post) { ?>
+								<li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+								</li>
+						<?php } ?>
+						</ul>
+				</div>	
 			</div>
+			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
